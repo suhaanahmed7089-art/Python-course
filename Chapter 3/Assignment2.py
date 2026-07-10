654e969fb08b917f4144c6117f5fe99deb86a4c9
Suhaan Ahmed <suhaanahmed7089@gmail.com>
2026-07-10 15:47:41 +0530

    Add script to print first and last character of name

---
+++ Chapter 3/Assignment2.py
@@ -0,0 +1,7 @@
+# Taking input from user for their name
+str=input("Enter your name: ")
+
+# Printing the first and last character of the user's name
+n= len(str)
+print("the first character of your name is: " , str[0])
+print("The last character of your name is: " , str[n-1])
