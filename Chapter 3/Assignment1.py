cb59ec5d0be83c01f8124926b0bd8d4818b769ee
Suhaan Ahmed <suhaanahmed7089@gmail.com>
2026-07-10 15:45:43 +0530

    Update input handling and string formatting
    Fix input prompt comment and improve formatting.

---
+++ Chapter 3/Assignment1.py
@@ -0,0 +1,6 @@
+# Taking inpu for a sentence
+str=input("Enter a sentence: ")
+
+# converting each letter of the sentence in lower case and replacing space between words with _ sign
+print(str.lower())
+print(str.replace(" ", "_"))
