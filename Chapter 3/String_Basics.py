e9d3578c55707d75d866f5bb3e97a9d15dea1580
Suhaan Ahmed <suhaanahmed7089@gmail.com>
2026-07-11 12:08:40 +0530

    Add String Basics example in String_Basics.py

---
+++ Chapter 3/String_Basics.py
@@ -0,0 +1,19 @@
+# creating strings
+
+str1 = 'Hello'
+str2 = "Suhaan Ahmed"
+str3 = '''My name is Suhaan Ahmed'''
+
+print(str1)
+print(str2)
+print(str3)
+
+# String concatenation
+
+print(str1 + " " + str2)
+
+# Length of a string
+
+print(len(str1))
+print(len(str2))
+print(len(str1 + " " + str2))
