61b6b4616bb9b263629e7ec450524ff814f0cb98
Suhaan Ahmed <suhaanahmed7089@gmail.com>
2026-07-11 12:07:56 +0530

    Add files via upload

---
+++ Chapter 3/Indexing.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+print(" Hello" + name + "! Welcome to python programing.")
+
+print("The length of your name is: " , len(name))
+print("The first character of your name is: " , name[0])
+print("The last character of your name is: " , name[len(name)-1]) 
