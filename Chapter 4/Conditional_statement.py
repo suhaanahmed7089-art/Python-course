14aae217d3b6f0988bf5ee3e7ad6b8f60d6d708e
Suhaan Ahmed <suhaanahmed7089@gmail.com>
2026-07-17 19:14:40 +0530

    Add conditional statements for grading system

---
+++ Chapter 4/Conditional_statement.py
@@ -0,0 +1,13 @@
+# Conditional satements
+marks=int(input("Enter your marks: "))
+
+if marks>90:
+    print("Grade A");
+elif marks>80:
+    print("Grade B");
+elif marks>70:
+    print("Grade C");
+elif marks>60:
+    print("Grade D");
+else:
+    print("Grade F");
