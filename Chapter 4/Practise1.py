4a49806af41f4d7412639b66d64baf40042fe0b9
Suhaan Ahmed <suhaanahmed7089@gmail.com>
2026-07-19 13:03:24 +0530

    Create a script to input and list three foods
    This script takes three food items as input from the user and stores them in a list, then prints the list and its length.

---
+++ Chapter 4/Practise1.py
@@ -0,0 +1,9 @@
+# take 3 foods as input and store them in a list
+
+food1=input("Enter food 1: ")
+food2=input("Enter food 2: ")
+food3=input("Enter food 3: ")
+
+foodlist= [food1,food2,food3]
+print(foodlist)
+print(len(foodlist))
