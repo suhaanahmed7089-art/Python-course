dec45dbd2b65432d54eb39a7c227bab179e7a3a2
Suhaan Ahmed <suhaanahmed7089@gmail.com>
2026-07-22 18:17:22 +0530

    Create Practise2.py for list to set example
    Add a script to demonstrate list to set conversion.

---
+++ Chapter 5/Practise2.py
@@ -0,0 +1,8 @@
+programminglist=["Python","Java","C++","Python","Java","C++"]
+print(type(programminglist))
+# Convert a list into set
+
+programmingset=set(programminglist)
+print(type(programmingset))
+print(programmingset)
+print(len(programmingset))
