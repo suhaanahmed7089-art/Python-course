8222bae8d77508db19663a55b272addafabcfdb2
Suhaan Ahmed <suhaanahmed7089@gmail.com>
2026-07-22 18:15:55 +0530

    Add Nested_Dictionary.py with user profile example

---
+++ Chapter 5/Nested_Dictionary.py
@@ -0,0 +1,11 @@
+# Nested dictionary
+
+profile={
+    "username":"SuhaanAhmed",
+    "details":{
+        "followers":156,
+        "Verified":"true"
+    }
+}
+print(profile)
+print(profile["details"])
