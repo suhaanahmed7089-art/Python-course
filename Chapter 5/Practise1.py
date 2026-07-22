1c420bdedb5d80661a4db9bc8081a32b8cfd5d53
Suhaan Ahmed <suhaanahmed7089@gmail.com>
2026-07-22 18:16:53 +0530

    Add Practise1.py to store and print subject marks

---
+++ Chapter 5/Practise1.py
@@ -0,0 +1,9 @@
+# Create a dictionary to store marks of three subjects and add the subjects one by one and print the final result
+
+marks={}
+print(type(marks))
+marks["maths"]=93
+marks["science"]=87
+marks["english"]=88
+
+print(marks)
