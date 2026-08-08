8afe263b0e44533ba6c189e23d3a892ee067719d
Suhaan Ahmed <suhaanahmed7089@gmail.com>
2026-08-08 18:37:44 +0530

    Add function to calculate average of two numbers

---
+++ Chapter 7/Function_Parameters.py
@@ -0,0 +1,10 @@
+# Function calling with arguments
+
+# Function definition with parameters
+def average(a,b):
+    avg=(a+b)/2
+    print(f"Num1 = {a} \nNum2 = {b} \nAverage = {avg}")
+
+# Calling functions with arguments
+average(10,20)
+average(5,7)
