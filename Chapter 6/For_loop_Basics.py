81f0ca1b09e5812753c54fdc5f54675aa037d4ed
Suhaan Ahmed <suhaanahmed7089@gmail.com>
2026-08-09 12:34:14 +0530

    Add For_loop_Basics.py with food and college lists

---
+++ Chapter 6/For_loop_Basics.py
@@ -0,0 +1,10 @@
+foodlist=["Cake","Mango","Pizza"]
+
+for i in foodlist:
+    print(i);
+
+college=("CGC Landran","NIT-Delhi","ABES","IIT")
+
+print("Colleges known by me")
+for j in college:
+    print(j)
