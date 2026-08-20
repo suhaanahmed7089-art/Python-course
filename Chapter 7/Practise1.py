d21c52e16d7c3f123d0f6090c9a2f7665de0ec41
Suhaan Ahmed <suhaanahmed7089@gmail.com>
2026-08-20 19:49:31 +0530

    Add welcome_message function to print greeting

---
+++ Chapter 7/Practise1.py
@@ -0,0 +1,7 @@
+# /A function named welcome_message that prints "Welcome to python programming!" three times.
+
+def welcome_message():
+    for i in range(3):
+      print("Welcome to python programming!")
+
+welcome_message()      # Function is being called here
