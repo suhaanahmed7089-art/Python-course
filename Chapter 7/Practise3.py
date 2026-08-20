4b4ebb7704b3f6d06f24169fb124879201da65e5
Suhaan Ahmed <suhaanahmed7089@gmail.com>
2026-08-20 19:51:00 +0530

    Add greet function to print greeting with name

---
+++ Chapter 7/Practise3.py
@@ -0,0 +1,8 @@
+# A function named greet that prints greeting with your name.
+
+def greet():
+    name=input("Enter your name: ")
+    print(f"Good Morning! {name}")
+
+greet()       # Function is being called here
+greet()       # Function is being called here
