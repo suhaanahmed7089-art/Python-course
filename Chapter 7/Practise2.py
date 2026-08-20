d15d2e8448159946b5dc0f5c6b340707cebbbe95
Suhaan Ahmed <suhaanahmed7089@gmail.com>
2026-08-20 19:49:55 +0530

    Add inspire function to print motivational quotes

---
+++ Chapter 7/Practise2.py
@@ -0,0 +1,8 @@
+# A function named inspire that prints a motivational quote with your name
+
+def inspire():
+    name=input("Enter your name: ")
+    quote=input("Enter a motivational quote: ")
+    print(f"\"{quote}\" - {name}")
+
+inspire()      #Function is being called here
