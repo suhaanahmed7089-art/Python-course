d3e41aac8afea2be08fc064aa6a1cc64e42fb217
Suhaan Ahmed <suhaanahmed7089@gmail.com>
2026-08-22 19:35:32 +0530

    Add login function to print user credentials

---
+++ Chapter 7/Practise10.py
@@ -0,0 +1,6 @@
+# Function named login(username,password=1234) that prints the credentials of the user.
+
+def login(username,password=1234):
+    print(f"Username: {username} \nPassword: {password}")
+
+login("Suhaan Ahmed",)
